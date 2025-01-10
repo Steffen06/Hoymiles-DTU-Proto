786f51af9a92f34223b30a4306e973665a5cdf50
Steffen06 <105735443+Steffen06@users.noreply.github.com>
2025-01-10 16:04:50 +0100

    added Wireshark plugin for live packet analys

---
+++ Wireshark/README.md
@@ -0,0 +1,61 @@
+# Wireshark plugin
+A Wireshark plugin to add decoding for data transmission between Hoymiles DTUs and the app/Hoymiles Cloud/custom application.
+
+
+### Disclaimer: 
+> [!NOTE]
+> This plugin is not affiliated with Wireshark or Hoymiles. It is an independent project developed to provide tools for easier analys of trafic with Hoymiles DTUs and devices connected to them. Any trademarks or product names mentioned are the property of their respective owners.
+
+
+This plugin is based on the Protobuf example from the [Wireshark Protobuf documentation](https://wiki.wireshark.org/Protobuf), where you can also find additional information and further explanations.
+
+
+## Protocol structure
+Hoymiles uses a proprietary implementation of the [Protobuf Protocol](https://protobuf.dev/) with an Header with additional raw Data about the following Data followed by Data encoded and serialized with the Protobuf Protocol. For mor details about the Protocol strukture see [protocol.md](../protocol.md). This approach has the advantage for the possibility for integrity check of the Data. But with the Disadvantage that a custom plugin like this is required.
+
+
+## Features / To-dos
+- [x] Basic decoding of all header fields
+- [x] Mapping the command ID to the correct Protobuf message type
+- [x] Decoding the Protobuf message with the associated Protobuf message type
+- [ ] Checking the length of the data based on the length in the header
+- [ ] Checking the crc16 against the Protobuf Data
+
+
+## Plugin installation
+
+1. Copy the [Plugin](hoymiles_protobuf_dissector.lua) into the [Plugin Folder from Wireshark](https://www.wireshark.org/docs/wsug_html_chunked/ChPluginFolders.html)
+2. Copy all [*.proto](protobuf) files into a (sub)folder (I would recommend putting the subfolder in the same location as the plugin)
+    - plugin dir
+        - protobuf_files (folder)
+            - *.proto files
+        - hoymiles_protobuf_dissector.lua
+3. Add the **folder** with all the *.proto to the Protobuf search Path
+    - `Edit` &rarr; `Preferences` &rarr; `Protocols` &rarr; `ProtoBuf` &rarr; `Protobuf search paths`
+4. Check correct Loading of the plugin
+    - `Help` &rarr; `About Wireshark` &rarr; `Plugins`
+
+Now you should see in protocol column `TCP_HOYMILES` instead of `TCP` in packets that contain Data.
+
+> [!TIP]
+> I would recommend to enable the following settings:
+> - Display JSON mapping for Protobuf message
+> - Show details of message, fields and enums
+
+> [!TIP]
+> If you want to filter only for packets with Data you can use the filter `_ws.col.protocol == "TCP_HOYMILES"`
+
+
+## Troubleshooting
+
+If it not appears in the list plugin list try reloding of Plugins:
+- `Analyze` &rarr; `Reload Lua Plugins (Ctrl-Shift-L)`
+- Restart Wireshark
+
+
+If the automatic adding of the decoder didn't work try adding it manually.
+- `Right click a Packet` &rarr; `Decode As...` &rarr; `Add an Entry with the following parameters`:
+
+| Field | Value | Type | Default | Current |
+| --- | --- | --- | --- | --- |
+| TCP port | 10081 | Integer, base 10 | (none) | TCP_HOYMILES |
---
+++ Wireshark/hoymiles_protobuf_dissector.lua
@@ -0,0 +1,80 @@
+do
+    local command_id_map = {
+        [0x2201] = "InfoDataReqDTO", [0x2301] = "InfoDataResDTO",
+        [0x2202] = "HBReqDTO", [0x2302] = "HBResDTO",
+        [0x2203] = "RealDataReqDTO", [0x2303] = "RealDataResDTO",
+        [0x2204] = "RealDataReqDTO", [0x2304] = "RealDataResDTO",
+        [0x2205] = "CommandReqDTO", [0x2305] = "CommandResDTO",
+        [0x2206] = "CommandStatusReqDTO", [0x2306] = "CommandStatusResDTO",
+        [0x2207] = "DevConfigFetchReqDTO", [0x2307] = "DevConfigFetchResDTO",
+        [0x2208] = "DevConfigPutReqDTO", [0x2308] = "DevConfigPutResDTO",
+        [0x220A] = "WaveReqDTO", [0x230A] = "WaveResDTO",
+        [0x220B] = "WarnReqDTO", [0x230B] = "WarnResDTO",
+        [0x220C] = "RealDataNewReqDTO", [0x230C] = "RealDataNewResDTO",
+        [0x220D] = "RealDataNewReqDTO", [0x230D] = "RealDataNewResDTO",
+
+        [0xA201] = "APPInfoDataReqDTO", [0xA301] = "APPInfoDataResDTO",
+        [0xA202] = "HBReqDTO", [0xA302] = "HBResDTO",
+        [0xA203] = "RealDataReqDTO", [0xA303] = "RealDataResDTO",
+        [0xA204] = "WarnReqDTO", [0xA304] = "WarnResDTO",
+        [0xA205] = "CommandReqDTO", [0xA305] = "CommandResDTO",
+        [0xA206] = "CommandStatusReqDTO", [0xA306] = "CommandStatusResDTO",
+        [0xA207] = "DevConfigFetchReqDTO", [0xA307] = "DevConfigFetchResDTO",
+        [0xA208] = "DevConfigPutReqDTO", [0xA308] = "DevConfigPutResDTO",
+        [0xA209] = "GetConfigReqDTO", [0xA309] = "GetConfigResDTO",
+        [0xA210] = "SetConfigReqDTO", [0xA310] = "SetConfigResDTO",
+        [0xA211] = "RealDataNewReqDTO", [0xA311] = "RealDataNewResDTO",
+        [0xA212] = "GPSTReqDTO", [0xA312] = "GPSTResDTO",
+        [0xA213] = "AutoSearchReqDTO", [0xA313] = "AutoSearchResDTO",
+        [0xA214] = "NetworkInfoReqDTO", [0xA314] = "NetworkInfoResDTO",
+        [0xA215] = "AppGetHistPowerReqDTO", [0xA315] = "AppGetHistPowerResDTO",
+        [0xA216] = "AppGetHistEDReqDTO", [0xA316] = "AppGetHistEDResDTO",
+    }
+    
+    
+    local protobuf_dissector = Dissector.get("protobuf")
+
+    -- Hoymiles protobuf protocol dissector
+    -- This dissector is based on the protobuf dissector
+    -- 2 Bytes Header HM (0x484D) as string
+    -- 2 Bytes Comand ID 
+    -- 2 Bytes Sequence ID
+    -- 2 Bytes Crc16 Checksum
+    -- 2 Bytes Length
+    -- n Bytes protobuf message
+
+    local proto_hoymiles = Proto("TCP_Hoymiles", "Hoymiles Protobuf Protocol")
+    local f_header = ProtoField.string("hoymiles.header", "Header")
+    local f_command_id = ProtoField.uint16("hoymiles.command_id", "Command ID", base.HEX)
+    local f_sequence_id = ProtoField.uint16("hoymiles.sequence_id", "Sequence ID", base.HEX)
+    local f_crc16 = ProtoField.uint16("hoymiles.crc16", "CRC16", base.HEX)
+    local f_length = ProtoField.uint16("hoymiles.length", "Length", base.DEC)
+
+    proto_hoymiles.fields = {f_header, f_command_id, f_sequence_id, f_crc16, f_length}
+    
+    function proto_hoymiles.dissector(buffer, pinfo, tree)
+        pinfo.cols.protocol = proto_hoymiles.name
+
+        local subtree = tree:add(proto_hoymiles, buffer(), "Hoymiles Protocol Data")
+        local header = buffer(0, 2)
+        subtree:add(f_header, header)
+        local command_id = buffer(2, 2)
+        local command_name = command_id_map[command_id:uint()]
+        subtree:add(f_command_id, command_id):append_text(" (" .. (command_name or "Unknown") .. ")")
+        local sequence_id = buffer(4, 2)
+        subtree:add(f_sequence_id, sequence_id)
+        local crc16 = buffer(6, 2)
+        subtree:add(f_crc16, crc16)
+        local length = buffer(8, 2)
+        subtree:add(f_length, length)
+        
+        -- Dissect the protobuf message
+        local payload = buffer(10):tvb()
+        if command_name then
+            pinfo.private["pb_msg_type"] = "message," .. command_name
+        end
+        protobuf_dissector:call(payload, pinfo, subtree)
+    end
+
+    DissectorTable.get("tcp.port"):add(10081, proto_hoymiles)
+end
